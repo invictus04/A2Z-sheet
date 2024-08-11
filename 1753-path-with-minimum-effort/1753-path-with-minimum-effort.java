f8688cae7a905c5edbd78d1decc5426d936e8714
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2024-08-11 20:58:03 +0530

    Time: 35 ms (99.08%), Space: 44.9 MB (79.44%) - LeetHub

---
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -0,0 +1,53 @@
+class Tuple{
+    int distance;
+    int row;
+    int col;
+    Tuple(int distance, int row, int col){
+        this.distance=distance;
+        this.row=row;
+        this.col=col;
+    }
+}
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((x,y) -> x.distance-y.distance);
+
+        int dis[][] = new int[n][m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                dis[i][j] = (int)(1e9);
+            }
+        }
+
+        dis[0][0]=0;
+        pq.add(new Tuple(0,0,0));
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+
+        while(!pq.isEmpty()){
+            Tuple it = pq.peek();
+            pq.remove();
+            int dist = it.distance;
+            int row = it.row;
+            int col = it.col;
+
+            if(row==n-1 && col == m-1) return dist;
+
+            for(int i=0; i<4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int maxEffect = Math.max(Math.abs(heights[row][col]-heights[nrow][ncol]), dist);
+                    if(maxEffect<dis[nrow][ncol]){
+                        dis[nrow][ncol] = maxEffect;
+                        pq.add(new Tuple(maxEffect,nrow,ncol));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
