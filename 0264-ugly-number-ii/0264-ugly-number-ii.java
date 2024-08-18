6632ccf63838908d7db3925b6027b342a99aaeed
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2024-08-18 21:12:13 +0530

    Time: 48 ms (23.24%), Space: 45.4 MB (5.92%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        TreeSet<Long> st = new TreeSet<>();
+        
+    /*  TreeSet<Long> st = new TreeSet<>();
         st.add(1L);
         Long ug = 1L;
         for(int i=0; i<n; i++){
@@ -10,5 +11,27 @@ class Solution {
             st.add(ug*5);
         }
         return ug.intValue();
+    */
+
+    PriorityQueue<Long> minHeap = new PriorityQueue<>();
+    Set<Long> seen = new HashSet<>();
+    int [] prime = {2,3,5};
+
+    minHeap.offer(1L);
+    seen.add(1L);
+
+    long ug = 1L;
+    for(int i =0; i<n; i++){
+        ug = minHeap.poll();
+
+        for(int p: prime){
+            long next = ug*p;
+            if(seen.add(next)){
+                minHeap.offer(next);
+            }
+        }
+    }
+
+    return (int) ug;
     }
 }
