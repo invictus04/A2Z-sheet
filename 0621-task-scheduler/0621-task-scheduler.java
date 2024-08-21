6a91b7f102a11e6efa374ed758978b6741e81452
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2024-08-22 01:28:58 +0530

    Time: 37 ms (16.59%), Space: 45.2 MB (92.72%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,47 @@
+class Task{
+    int time;
+    int count;
+    Task(int time, int count){
+        this.time = time;
+        this.count= count;
+    }
+}
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if(n == 0) return tasks.length;
+        int time =0;
+        int[] freq = new int[26];
+        PriorityQueue<Task> pq = new PriorityQueue<>(
+            new Comparator<Task>(){
+                public int compare(Task t1, Task t2){
+                    return -Integer.compare(t1.count,t2.count);
+                }
+            });
+        for(char ch : tasks){
+            freq[ch - 'A']++;
+        }
+
+        for(int i=0; i<26; i++){
+            if(freq[i]>=1){
+                pq.add(new Task(0,freq[i]));
+            }
+        }
+        Queue<Task> q = new LinkedList<>();
+        while(!pq.isEmpty() || !q.isEmpty()){
+            if(!q.isEmpty() && time-q.peek().time>n){
+                pq.add(q.remove());
+            }
+
+            if(!pq.isEmpty()){
+                Task t = pq.remove();
+                t.count--;
+                t.time = time;
+                if(t.count >0){
+                    q.add(t);
+                }
+            }
+            time++;
+        }
+    return time;
+    }
+}
