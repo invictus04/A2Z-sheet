235ecc3bf7553c92f383c2517724c5955f21d5c7
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2024-09-01 19:07:51 +0530

    Time: 0 ms (100%), Space: 42 MB (100%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.java
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.java
@@ -4,8 +4,8 @@ class Solution {
         int ch2 = coordinate2.charAt(0) - 'a'+ 1;
         int num1 = coordinate1.charAt(1)-'0';
         int num2 = coordinate2.charAt(1)- '0';
-       if((ch1+num1)%2 == 0 && (ch2+num2)%2 == 0) return true;
-       if((ch1+num1)%2 != 0 && (ch2+num2)%2 != 0) return true;
+       if((ch1+num1)%2 == 0 && (ch2+num2)%2 == 0 || (ch1+num1)%2 != 0 && (ch2+num2)%2 != 0) return true;
+    //    if((ch1+num1)%2 != 0 && (ch2+num2)%2 != 0) return true;
        return false;
     }
 }
