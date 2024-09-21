f49829982ddd9e2752e9cabe8d1145a7a3218882
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2024-09-21 12:52:10 +0530

    Time: 72 ms (27.08%), Space: 80.1 MB (5.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,35 @@
 class Solution {
-    private int helper(int[] nums, int n, int ind, int prev, int dp[][]){
-        if(ind == n) return 0;
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
-        int notTake = helper(nums,n,ind+1,prev,dp);
-        int take=0;
-        if(prev == -1 || nums[prev] < nums[ind]){
-            take = 1 + helper(nums,n,ind+1,ind,dp);
+    private int helper(int[] nums, int n, int ind, int prev, int dp[][]) {
+        if (ind == n)
+            return 0;
+        if (dp[ind][prev + 1] != -1)
+            return dp[ind][prev + 1];
+        int notTake = helper(nums, n, ind + 1, prev, dp);
+        int take = 0;
+        if (prev == -1 || nums[prev] < nums[ind]) {
+            take = 1 + helper(nums, n, ind + 1, ind, dp);
         }
-        return dp[ind][prev+1] = Math.max(notTake,take);
+        return dp[ind][prev + 1] = Math.max(notTake, take);
     }
+
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int dp[][] = new int[n][n+1];
-        for(int row[]: dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[n + 1][n + 1];
+        // for(int row[]: dp){
+        // Arrays.fill(row,-1);
+        // }
+        // return helper(nums,n,0,-1,dp);
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int prev = ind - 1; prev >= -1; prev--) {
+                int notTake = dp[ind + 1][prev + 1];
+                int take = Integer.MIN_VALUE;
+                if (prev == -1 || nums[prev] < nums[ind]) {
+                    take = 1 + dp[ind + 1][ind + 1];
+                }
+                dp[ind][prev + 1] = Math.max(notTake, take);
+            }
         }
-        return helper(nums,n,0,-1,dp);
+        return dp[0][0];
     }
 }
