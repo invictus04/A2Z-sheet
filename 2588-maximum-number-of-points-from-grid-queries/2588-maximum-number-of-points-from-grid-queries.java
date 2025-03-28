60de7d02adc2d7d3675516be30ebd888ebf705d4
Rajvardhan Singh <97658238+invictus04@users.noreply.github.com>
2025-03-29 00:42:08 +0530

    Time: 55 ms (88.55%), Space: 56.4 MB (78.63%) - LeetHub

---
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.java
@@ -0,0 +1,74 @@
+class Solution {
+
+    public static final int[][] DIRECTIONS = {
+        { 1, 0 },
+        { -1, 0 },
+        { 0, 1 },
+        { 0, -1 },
+    };
+
+    public int[] maxPoints(int[][] grid, int[] queries) {
+        int queryCount = queries.length;
+        int[] result = new int[queryCount];
+        int rowCount = grid.length;
+        int colCount = grid[0].length;
+
+        int totalCells = rowCount * colCount;
+        int[] thresholdForMaxPoints = new int[totalCells + 1];
+        int[][] minValueToReach = new int[rowCount][colCount];
+
+        for (int[] row : minValueToReach) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        minValueToReach[0][0] = grid[0][0];
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) ->
+            Integer.compare(a[2], b[2])
+        );
+        minHeap.offer(new int[] { 0, 0, grid[0][0] });
+        int visitedCells = 0;
+        while (!minHeap.isEmpty()) {
+            int[] current = minHeap.poll();
+            thresholdForMaxPoints[++visitedCells] = current[2];
+
+            for (int[] direction : DIRECTIONS) {
+                int newRow = current[0] + direction[0];
+                int newCol = current[1] + direction[1];
+                if (
+                    newRow >= 0 &&
+                    newRow < rowCount &&
+                    newCol >= 0 &&
+                    newCol < colCount &&
+                    minValueToReach[newRow][newCol] == Integer.MAX_VALUE
+                ) {
+                    minValueToReach[newRow][newCol] = Math.max(
+                        current[2],
+                        grid[newRow][newCol]
+                    );
+
+                    minHeap.offer(
+                        new int[] {
+                            newRow,
+                            newCol,
+                            minValueToReach[newRow][newCol],
+                        }
+                    );
+                }
+            }
+        }
+        for (int i = 0; i < queryCount; i++) {
+            int threshold = queries[i];
+            int left = 0, right = totalCells;
+            while (left < right) {
+                int mid = (left + right + 1) >>> 1;
+                if (thresholdForMaxPoints[mid] < threshold) {
+                    left = mid;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            result[i] = left;
+        }
+
+        return result;
+    }
+}
